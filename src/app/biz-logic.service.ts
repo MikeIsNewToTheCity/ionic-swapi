a247f3a42a218dc3cf74c45738b9d3737e42c16f
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-11-24 10:50:35 -0600

    Refactored business logic into service

---
+++ src/app/biz-logic.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BizLogicService {
+
+  constructor() { }
+
+  getDisplayColor(planet) {
+    return planet.name == 'unknown' ? 
+      'goldenrod' :
+        planet.name == 'Tatooine' ?
+          'green' : 
+          'inherit'
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SwapiService } from '../swapi.service';
+import { BizLogicService } from '../biz-logic.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,10 @@ export class HomePage {
 
   planets = [];
 
-  constructor(private swapiSvc: SwapiService) {
+  constructor(
+    private swapiSvc: SwapiService
+    , private bizLogicSvc: BizLogicService
+  ) {
     this.swapiSvc
       .fetchPlanets()
       .subscribe(
@@ -19,18 +23,12 @@ export class HomePage {
           , ...data
         ].map(x => ({
           ...x
-          , displayColor: this.getDisplayColor(x)
+          , displayColor: this.bizLogicSvc.getDisplayColor(x)
         })).sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1)
         , err => console.error(err)
       );
   }
 
-  getDisplayColor(planet) {
-    return planet.name == 'unknown' ? 
-      'goldenrod' :
-        planet.name == 'Tatooine' ?
-          'green' : 
-          'inherit'
-  }
+
 
 }
